32c9474467d8592de84bc0ea752f7cd9e63bd9e9
Rafael José Mecenas Silva <100150189+RafaJMS@users.noreply.github.com>
2023-12-21 17:24:18 -0300

    Add files via upload

---
+++ ToDo - Fazer/assets/js/script.js
@@ -0,0 +1,55 @@
+function botaoFechar(li)
+{
+    let span = document.createElement("span")
+    let txt = document.createElement("text")
+    let boxTarefas = document.getElementById('lista-tarefas')
+    let countTarefas = document.querySelectorAll("li")
+
+    
+
+    span.setAttribute('class','close')
+    span.setAttribute('id','excluir')
+    span.appendChild(txt)
+    txt.textContent = "\uD83D\uDDF5"
+    
+    li.appendChild(span)
+    span.onclick = () => {
+            span.parentElement.style.display = 'none'
+    }
+}
+
+document.querySelectorAll('li').forEach(botaoFechar);
+
+document.querySelector('ol').addEventListener('click',(e)=>{
+    if(e.target.tagName === 'LI'){
+        e.target.classList.toggle('checked');
+    }
+})
+
+function addTarefa()
+{
+let li = document.createElement('li')
+let tarefa = document.form_main.task.value
+let caixaTarefa = document.createTextNode(tarefa)
+let boxTarefas = document.getElementById('lista-tarefas')
+
+if(tarefa == null || tarefa == '' ){
+    return
+}else{
+    boxTarefas.style.display = 'flex'
+    li.appendChild(caixaTarefa)
+    document.querySelector('ol').appendChild(li)
+    document.form_main.task.value = ''
+    
+    botaoFechar(li)
+}
+}
+
+document.addEventListener("keydown", function(e) {
+    
+    if (e.key === "Enter") {
+        addTarefa()
+        e.preventDefault()
+    }
+        return
+  });
